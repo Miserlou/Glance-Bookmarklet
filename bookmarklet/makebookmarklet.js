e2db2d9a6e200bb4e46f551d8f0275ae93c13c2a
Spencer Cohen <sc@moosoftware.com>
2014-02-27 18:09:06 -0500

    Remove bookmarklet to put it in its own branch for separate pull request

--- bookmarklet/makebookmarklet.js
+++
@@ -1,9 +0,0 @@
-(function (){
-    var fs = require('fs');
-    var uglify = require('uglify-js');
-    //Parse arguments
-    var code = uglify.minify(['../spritz.js', 'inject.js']).code
-    var bookmarklet = 'javascript:void(function(){' + code + '}())';
-    fs.writeFileSync('bookmarklet.txt', bookmarklet);
-    console.log('Done.');
-})();
