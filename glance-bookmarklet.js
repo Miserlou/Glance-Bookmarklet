962a49f44441679f73b313cff0cb032de1d51e6c
Yuval Langer <yuval.langer@gmail.com>
2014-09-13 18:06:30 +0300

    Replacing all occurences of Spritz with Glance Bookmarklet
    The bookmarklet as is in the master branch is broken. It is refering to
    the old repositry within the code.
    
    I have changed all mentions of Spritz into "glance bookmarklet" while
    trying to keep coding style consistent.
    I have used glanceBookmarklet etc. where camelCase is used and
    glance_bookmarklet where underscore is used.
    
    The README file still needs work.

--- glance-bookmarklet.js
+++ glance-bookmarklet.js
@@ -1,46 +1,49 @@
-// spritz.js
+// glance-bookmarklet.js
 // A JavaScript Speed Reader
 // rich@gun.io
-// https://github.com/Miserlou/OpenSpritz
+// https://github.com/Miserlou/Glance-Bookmarklet
 
 // Please don't abuse this.
 var readability_token = '172b057cd7cfccf27b60a36f16b1acde12783893';
 var diffbot_token = '2efef432c72b5a923408e04353c39a7c';
 
-function create_spritz(){
+function createGlanceBookmarklet(){
 
-     spritz_loader = function() {
-        //getURL("https://rawgithub.com/Miserlou/OpenSpritz/master/spritz.html", function(data){
+    glanceBookmarkletLoader = function() {
+        // XXX RawGit's CDN usage:
+           // "Since files are not refreshed after the first request,
+           // it's best to use a specific tag or commit URL, not a branch URL."
 
-        //getURL("https://rawgithub.com/Miserlou/OpenSpritz/dev/spritz.html", function(data){
+        // Make sure to comment out / in the right getURL line.
 
-        // This won't work in Firefox because an old bug and won't work in Chrome because of security stuff:
-        //getURL("spritz.html", function(data){
+        // XXX This won't work in Firefox because an old bug and won't work in Chrome because of security stuff:
+        //getURL("glance-bookmarklet.html", function(data){
 
-        //getURL("https://rawgithub.com/Miserlou/OpenSpritz/dev/spritz.html", function(data){
+        //getURL("https://rawgithub.com/Miserlou/Glance-Bookmarklet/master/glance-bookmarklet.html", function(data){
 
-        // RawGit's CDN usage:
-        // "Since files are not refreshed after the first request,
-        // it's best to use a specific tag or commit URL, not a branch URL."
-        getURL("https://cdn.rawgit.com/Miserlou/OpenSpritz/9e92c605032be16c986ed699d68e0acd3534e6b1/spritz.html", function(data){
-            var spritzContainer = document.getElementById("spritz_container");
+        getURL("https://rawgithub.com/Miserlou/Glance-Bookmarklet/dev/glance-bookmarklet.html", function(data){
 
-            if (!spritzContainer) {
+        // XXX Make sure to replace the commit hash with the wanted one.
+        //getURL("https://cdn.rawgit.com/Miserlou/Glance-Bookmarklet/9e92c605032be16c986ed699d68e0acd3534e6b1/glance-bookmarklet.html", function(data){
+
+            var glanceBookmarkletContainer = document.getElementById("glance_bookmarklet_container");
+
+            if (!glanceBookmarkletContainer) {
                 var ele = document.createElement("div");
                 data = data.replace(/(\r\n|\n|\r)/gm,"");
                 ele.innerHTML = data;
                 document.body.insertBefore(ele, document.body.firstChild);
-                document.getElementById("spritz_toggle").style.display = "none";
+                document.getElementById("glance_bookmarklet_toggle").style.display = "none";
             };
 
-            document.getElementById("spritz_selector").addEventListener("change", function(e) {
+            document.getElementById("glance_bookmarklet_selector").addEventListener("change", function(e) {
                 clearTimeouts();
-                spritz();
+                glanceBookmarklet();
             });
         });
     };
 
-    spritz_loader();
+    glanceBookmarkletloader();
 }
 
 function getURL(url, callback) {
@@ -56,34 +59,34 @@ function getURL(url, callback) {
     xmlhttp.send();
 }
 
-function hide_spritz(){
-    document.getElementById("spritz_spacer").style.display = "none";
-    document.getElementById("spritz_container").style.display = "none";
-    document.getElementById("spritz_holder").style.display = "none";
+function hideGlanceBookmarklet(){
+    document.getElementById("glance_bookmarklet_spacer").style.display = "none";
+    document.getElementById("glance_bookmarklet_container").style.display = "none";
+    document.getElementById("glance_bookmarklet_holder").style.display = "none";
 }
 
 // Entry point to the beef.
 // Gets the WPM and the selected text, if any.
-function spritz(){
+function glanceBookmarklet(){
 
-    var wpm = parseInt(document.getElementById("spritz_selector").value, 10);
+    var wpm = parseInt(document.getElementById("glance_bookmarklet_selector").value, 10);
     if(wpm < 1){
         return;
     }
 
     var selection = getSelectionText();
     if(selection){
-        spritzify(selection);
+        glanceBookmarkletify(selection);
     }
     else{
-        spritzifyURL();
+        glanceBookmarkletifyURL();
     }
 }
 
 // The meat!
-function spritzify(input){
+function glanceBookmarkletify(input){
 
-    var wpm = parseInt(document.getElementById("spritz_selector").value, 10);
+    var wpm = parseInt(document.getElementById("glance_bookmarklet_selector").value, 10);
     var ms_per_word = 60000/wpm;
 
     // Split on any spaces.
@@ -139,51 +142,51 @@ function spritzify(input){
 
     var currentWord = 0;
     var running = true;
-    var spritz_timers = new Array();
+    var glanceBookmarkletTimers = new Array();
 
-    document.getElementById("spritz_toggle").addEventListener("click", function() {
+    document.getElementById("glance_bookmarklet_toggle").addEventListener("click", function() {
         if(running) {
-            stopSpritz();
+            stopGlanceBookmarklet();
         } else {
-            startSpritz();
+            startGlanceBookmarklet();
         }
     });
 
     function updateValues(i) {
 
         var p = pivot(all_words[i]);
-        document.getElementById("spritz_result").innerHTML = p;
+        document.getElementById("glance_bookmarklet_result").innerHTML = p;
         currentWord = i;
 
     }
 
-    function startSpritz() {
+    function startGlanceBookmarklet() {
 
-        document.getElementById("spritz_toggle").style.display = "block";
-        document.getElementById("spritz_toggle").textContent = "Pause";
+        document.getElementById("glance_bookmarklet_toggle").style.display = "block";
+        document.getElementById("glance_bookmarklet_toggle").textContent = "Pause";
 
         running = true;
 
-        spritz_timers.push(setInterval(function() {
+        glanceBookmarkletTimers.push(setInterval(function() {
             updateValues(currentWord);
             currentWord++;
             if(currentWord >= all_words.length) {
                 currentWord = 0;
-                stopSpritz();
+                stopGlanceBookmarklet();
             }
         }, ms_per_word));
     }
 
-    function stopSpritz() {
-        for(var i = 0; i < spritz_timers.length; i++) {
-            clearTimeout(spritz_timers[i]);
+    function stopGlanceBookmarklet() {
+        for(var i = 0; i < glanceBookmarkletTimers.length; i++) {
+            clearTimeout(glanceBookmarkletTimers[i]);
         }
 
-        document.getElementById("spritz_toggle").textContent = "Play";
+        document.getElementById("glance_bookmarklet_toggle").textContent = "Play";
         running = false;
     }
 
-    startSpritz();
+    startGlanceBookmarklet();
 }
 
 // Find the red-character of the current word.
@@ -223,10 +226,10 @@ function pivot(word){
     var end = word.slice(bestLetter, length).replace('.', '&#8226;') + '.'.repeat((11-(word.length-bestLetter)));
 
     var result;
-    result = "<span class='spritz_start'>" + start;
-    result = result + "</span><span class='spritz_pivot'>";
+    result = "<span class='glance_bookmarklet_start'>" + start;
+    result = result + "</span><span class='glance_bookmarklet_pivot'>";
     result = result + middle;
-    result = result + "</span><span class='spritz_end'>";
+    result = result + "</span><span class='glance_bookmarklet_end'>";
     result = result + end;
     result = result + "</span>";
 
@@ -262,7 +265,7 @@ function getSelectionText() {
 }
 
 // Uses the Readability API to get the juicy content of the current page.
-function spritzifyURL(){
+function glanceBookmarkletifyURL(){
     var url = document.URL;
 
     //getURL("https://www.readability.com/api/content/v1/parser?url="+ encodeURIComponent(url) +"&token=" + readability_token +"&callback=?",
@@ -272,7 +275,7 @@ function spritzifyURL(){
             data = JSON.parse(data);
 
             if(data.error){
-                document.getElementById("spritz_result").innerText = "Article extraction failed. Try selecting text instead.";
+                document.getElementById("glance_bookmarklet_result").innerText = "Article extraction failed. Try selecting text instead.";
                 return;
             }
 
@@ -294,7 +297,7 @@ function spritzifyURL(){
             text_content = text_content.replace(/\./g, '. '); // Make sure punctuation is apprpriately spaced.
             text_content = text_content.replace(/\?/g, '? ');
             text_content = text_content.replace(/\!/g, '! ');
-            spritzify(text_content);
+            glanceBookmarkletify(text_content);
         });
 
 }
